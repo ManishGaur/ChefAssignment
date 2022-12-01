097b00ba365b2bbc939ba29a5f96c4853c18127c
root <root@ip-172-31-12-179.ap-south-1.compute.internal>
2022-12-01 06:06:19 +0000

    chef-repo solo

--- day4/loop.rb
+++
@@ -1,5 +0,0 @@
-['welcome', 'to', 'the','loop'].each do |str|
-	log " logging :"+str do
-		level :info
-	end
-end
--- day4/onlyif.rb
+++
@@ -1,4 +0,0 @@
-log "Welcome to Chef, platform is #{node['platform']}!" do
-		level :info
-		only_if {node['platform'] == 'debian'}
-end
